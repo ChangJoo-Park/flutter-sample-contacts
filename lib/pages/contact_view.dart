c9d2fc0bb0e8b6e585ecf7bb459f41feb6d03454
ChangJoo Park <pcjpcj2@gmail.com>
2018-03-29 08:08:47 +0900

    Add drawer and navigation

---
+++ lib/pages/contact_view.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import '../models/contact_data.dart';
+
+class ContactsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Contacts"),
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(10.0),),
+              Text('Hello World')
+            ],
+          ),
+        ),
+        body: Container(
+          child: ContactList(kContacts),
+        ));
+  }
+}
+
+class ContactList extends StatelessWidget {
+  final List<Contact> _contacts;
+
+  ContactList(this._contacts);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      itemCount: _contacts.length,
+      itemBuilder: _buildContacts,
+    );
+  }
+
+  Widget _buildContacts(context, index) {
+    return ContactItem(_contacts[index]);
+  }
+}
+
+class ContactItem extends StatelessWidget {
+  const ContactItem(this.contact);
+
+  final Contact contact;
+
+  Widget _buildTiles(BuildContext context, Contact contact) {
+    return ListTile(
+      title: Text(contact.fullName),
+      subtitle: Text(contact.email),
+      leading: CircleAvatar(
+        child: Text(contact.fullName[0]),
+      ),
+      trailing: Icon(Icons.card_travel),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(contact.fullName)
+                ),
+                body: Text('Hello'),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(context, contact);
+  }
+}
